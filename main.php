ee1eee30e7ed1051f7a6ae41f78867c02dbc4790
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-02-08 23:27:32 +0200

    Progress, now it have some features, especially: hover img, hover user pfp

--- main.php
+++ main.php
@@ -26,7 +26,53 @@
         <div class="main1">
 
             <div class="posts">
-                fwfwgegrtgergregrew
+                <div class="post">
+                    <div class="user-post">
+                        <img class="user-pfp" src="http://img1.joyreactor.cc/pics/avatar/user/801497">
+                        <div class="drop">
+                            
+                        </div>
+                        <div class="user-username">
+                            Virgo
+                        </div>
+                    </div>
+                    <img class="post-image" id="myImg" alt="KEKW" src=http://img10.joyreactor.cc/pics/post/%D0%B0%D1%80%D1%82-%D0%B1%D0%B0%D1%80%D1%8B%D1%88%D0%BD%D1%8F-art-Nell-Nelson-6479567.jpeg>
+                    
+                    <!-- Modal -->
+
+                    <div id="myModal" class="modal">
+                        <span class="close">&times;</span>
+                        <img class="modal-content" id="img01">
+                        <div id="caption"></div>
+                    </div>
+
+                    <script>
+                        // Get the modal
+                        var modal = document.getElementById('myModal');
+
+                        // Get the image and insert it inside the modal - use its "alt" text as a caption
+                        var img = document.getElementById('myImg');
+                        var modalImg = document.getElementById("img01");
+                        var captionText = document.getElementById("caption");
+                        img.onclick = function(){
+                        modal.style.display = "block";
+                        modalImg.src = this.src;
+                        captionText.innerHTML = this.alt;
+                        }
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                        modal.style.display = "none";
+                        }
+
+                    </script>
+
+                    <!-- Modal -->
+
+                </div>
             </div>
 
             <div class="tags">
