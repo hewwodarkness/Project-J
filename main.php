1cffe0e4460ce2aeda44eedd4decaa67d75064dd
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-02-08 22:40:44 +0200

    Starting a project

---
+++ main.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <div class="menu">
+
+        <div class="url">
+            Joyreactor
+        </div>
+
+    </div>
+
+
+    <div class="intro">
+
+        <div></div>
+        <div></div>
+
+        <div class="main1">
+
+            <div class="posts">
+                fwfwgegrtgergregrew
+            </div>
+
+            <div class="tags">
+                fwfdw
+            </div>  
+
+        
+        </div>
+
+
+    </div>
+
+
+
+</body>
+</html>
+
+
