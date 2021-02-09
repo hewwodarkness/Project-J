7f60da8c58da826201070c44b491f1380ff21e39
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-02-09 18:21:32 +0200

    30% of main page is now complete. Example is done

--- main.php
+++ main.php
@@ -28,12 +28,18 @@
             <div class="posts">
                 <div class="post">
                     <div class="user-post">
-                        <img class="user-pfp" src="http://img1.joyreactor.cc/pics/avatar/user/801497">
-                        <div class="drop">
-                            
-                        </div>
+                        <img class="user-pfp" src="http://img0.joyreactor.cc/pics/avatar/user/310186">
+                        
                         <div class="user-username">
-                            Virgo
+                            <p>No_feelings</p>
+                        </div>
+                    </div>
+                    <div class="post-tags">
+                        <div class="post-tag">
+                            Anime
+                        </div>
+                        <div class="post-tag">
+                            Girl
                         </div>
                     </div>
                     <img class="post-image" id="myImg" alt="KEKW" src=http://img10.joyreactor.cc/pics/post/%D0%B0%D1%80%D1%82-%D0%B1%D0%B0%D1%80%D1%8B%D1%88%D0%BD%D1%8F-art-Nell-Nelson-6479567.jpeg>
@@ -72,26 +78,78 @@
 
                     <!-- Modal -->
 
-                </div>
-                
-                <div class="post-comments">
 
+                    <div class="post-comments">
+                        <p>Comments</p>
+                    </div>
                 </div>
+
+                <div class="post">
+                    <div class="user-post">
+                        <img class="user-pfp" src="http://img0.joyreactor.cc/pics/avatar/user/310186">
+                        
+                        <div class="user-username">
+                            <p>No_feelings</p>
+                        </div>
+                    </div>
+                    <div class="post-tags">
+                        <div class="post-tag">
+                            Anime
+                        </div>
+                        <div class="post-tag">
+                            Girl
+                        </div>
+                    </div>
+                    <img class="post-image" id="myImg" alt="KEKW" src=http://img10.joyreactor.cc/pics/post/Anime-t-bone-%2806tbone%29-Ruler-%28FateApocrypha%29-FateApocrypha-6480014.jpeg>
+                    
+                    <!-- Modal -->
+
+                    <div id="myModal" class="modal">
+                        <span class="close">&times;</span>
+                        <img class="modal-content" id="img01">
+                        <div id="caption"></div>
+                    </div>
+
+                    <script>
+                        // Get the modal
+                        var modal = document.getElementById('myModal');
+
+                        // Get the image and insert it inside the modal - use its "alt" text as a caption
+                        var img = document.getElementById('myImg');
+                        var modalImg = document.getElementById("img01");
+                        var captionText = document.getElementById("caption");
+                        img.onclick = function(){
+                        modal.style.display = "block";
+                        modalImg.src = this.src;
+                        captionText.innerHTML = this.alt;
+                        }
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                        modal.style.display = "none";
+                        }
+
+                    </script>
+
+                    <!-- Modal -->
+
+
+                    <div class="post-comments">
+                        <p>Comments</p>
+                    </div>
+                </div>
+
+                
             </div>
 
             <div class="tags">
-                <div class="tag-anime">
-                        
-                </div>
-                <div class="tag-anime">
-                        
-                </div>
-                <div class="tag-anime">
-                        
-                </div>
-                <div class="tag-anime">
-                        
-                </div>
+            <img class="tag-anime" src="1.jpg">
+            <img class="tag-anime" src="1.jpg">
+            <img class="tag-anime" src="1.jpg">
+            <img class="tag-anime" src="1.jpg">
             </div>  
 
         
