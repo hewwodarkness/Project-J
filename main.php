315758699bc3876df6ccd06ff2c631aaae753df5
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-02-13 13:52:29 +0200

    Register, login is now here(no security but still its available)

--- main.php
+++ main.php
@@ -58,7 +58,18 @@
 
         <div></div>
         <div></div>
-
+        <?php
+            session_start();
+            if (!$_SESSION['user']) {
+                header('Location: /');
+            }
+        ?>
+        <form>
+            <img src="<?= $_SESSION['user']['avatar'] ?>" width="200" alt="">
+            <h2 style="margin: 10px 0;"><?= $_SESSION['user']['full_name'] ?></h2>
+            <a href="#"><?= $_SESSION['user']['email'] ?></a>
+            <a href="vendor/logout.php" class="logout">Выход</a>
+        </form>
         <div class="main1">
             <div class="posts">
                 <div class="post">
