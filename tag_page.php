1ed8d4dbeb65fc821e57e94a2a33edf0af92eae9
hewwodarkness <66019326+shuvaIov@users.noreply.github.com>
2021-02-27 14:40:19 +0200

    New features: moderators and medals in tag. Works absolutely fine

--- tag_page.php
+++ tag_page.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/user_profile.css">
-    <title>User Page</title>
+    <link rel="stylesheet" href="css/tag_page.css">
+    <title>Tag Page</title>
 </head>
 <body>
     <?php
@@ -25,7 +26,9 @@
 <?php 
     include("db_conn1.php");
     $id = $_GET['tag_id'];
-
+// 
+// 
+// 
     $sql_select4 =  "SELECT distinct tag_id, tag_name
                     FROM tags
                     WHERE tag_id ='$id'
@@ -33,8 +36,47 @@
 
     $result4 = mysqli_query($conn, $sql_select4);
     $row4 = mysqli_fetch_array($result4, MYSQLI_ASSOC);
+// 
+// 
+// 
+    $sql_select99 =    "SELECT distinct u.id, u.full_name, u.avatar, t.dateJoined
+                        From users u
+                        INNER JOIN tags_moderators t
+                        ON u.id = t.user_id
+                        WHERE tag_id ='$id'
+                        ";
+
+    $result99 = mysqli_query($conn, $sql_select99);
+
+    if (!$result99) {
+        die('Invalid query: ' . mysqli_error($conn));
+    }
+
+    $row99 = mysqli_fetch_all($result99, MYSQLI_ASSOC);
 
 
+    // 
+    // 
+    // 
+
+    $sql_select98 =    "SELECT distinct u.medal_id, u.medal_name, u.medal_avatar
+                        From tags_medals u
+                        INNER JOIN tags t
+                        ON u.tag_id = t.tag_id
+                        WHERE u.tag_id ='$id'
+                        ";
+
+    $result98 = mysqli_query($conn, $sql_select98);
+
+    if (!$result98) {
+        die('Invalid query: ' . mysqli_error($conn));
+    }
+
+    $row98 = mysqli_fetch_all($result98, MYSQLI_ASSOC);
+
+// 
+// 
+// 
     $sql_select =  "SELECT distinct p.post_id, p.user_id, p.text, p.image, p.dateCreated, p.comment_id
                     FROM post p
                     INNER JOIN post_tags t
@@ -68,7 +110,44 @@
     <div class="intro">
 
 <div></div>
-<div></div>
+
+<div class="tag-information">
+
+    <div class="tag-information-top">
+
+        <div>
+            <img class="tag-information-image" src="uploads/tenor.gif" alt="">
+        </div>
+
+<!--  -->
+<!-- nlf means name, link and followers -->
+<!--  -->
+
+        <div class="tag-information-nlf"> 
+
+            <div class="tag-information-name">
+                <?=$row4['tag_name']?>
+            </div>
+
+            <p class="user-info-link">
+                u/userlink
+            </p>
+
+            <div class="tag-information-followers">
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="tag-information-text">
+
+    
+    </div>
+
+</div>
+
 <div class="main1">
     <div class="posts">
         <div class="post">
@@ -364,7 +443,85 @@
                 <p class="user-info-link">
                     u/userlink
                 </p>
-        </div>  
+        </div> 
+        <div class="right-block-moderators">
+
+            <p class="right-block-moderators-name">
+                Moderators:
+            </p>
+
+            <div class="right-block-moderators-list">
+                <?php foreach($row99 as $row99): ?>
+                    <div class="right-block-moderators-each">
+
+
+                            <img class="right-block-moderators-each-pfp"
+                                src="
+                                    <?=$row99['avatar']?>
+                                ">
+
+                        <div class="right-block-moderators-each-name">
+                            <p>
+                                <?=$row99['full_name']?>
+                            </p>
+                            <p class="right-block-moderators-each-name-date">
+                               From: <?=$row99['dateJoined']?>
+                            </p>
+
+                        </div>
+                    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js">
+                        $(document).ready(function() {
+                            $(".right-block-moderators-each-name").each(function() {
+                                var targetHeight = $(this).clone().html("&nbsp;").insertAfter($(this));
+
+                                while ($(this).height() > targetHeight.height()) {
+                                    $(this).css("font-size", parseInt($(this).css("font-size")) - 1);
+                                }
+
+                                targetHeight.remove();
+                            });
+                        });
+                    </script> -->
+
+                       
+
+                   
+                    </div>
+                    
+                <?php endforeach; ?>
+                
+            </div>
+        </div>
+
+        <div class="right-block-avaiable-medals">
+            <p>
+                Medals you can get:
+            </p>
+
+            <div class="right-block-avaiable-medals-list">
+                <?php foreach($row98 as $row98): ?>
+                    <div class="right-block-avaiable-medals-each">
+
+                        <div>
+
+                            <img class="user-pfp"
+                                src="
+                                    <?=$row98['medal_avatar']?>
+                                ">
+                        </div>
+
+                        <div class="right-block-avaiable-medals-each-name">
+                            <?=$row98['medal_name']?>
+                        </div>
+
+                   
+                    </div>
+                    
+                <?php endforeach; ?>
+                
+            </div> 
+
+        </div>
     </div>
 
 
